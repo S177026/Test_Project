e3732e3486cbdfacb853aed1c16bcafc8972332d
Lewis <S177026@ucs.ac.uk>
2017-09-27 16:18:23 +0100

    Group Game Test
    Camera and Light Test

---
+++ Test_Project/Assets/Color_Change.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Color_Change : MonoBehaviour {
+    public Light lt;
+    void Start() {
+        lt = GetComponent<Light>();
+    }
+    void Update() {
+
+    }
+
+	void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Blue_PickUp"))
+        lt.color -= Color.blue;
+        Destroy(other.gameObject);
+}
+}
